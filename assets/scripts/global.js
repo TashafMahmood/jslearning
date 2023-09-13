5be65969303ea4ff844871b73a15e8fc1fbee304
TashafMahmood <tashaf@elred.io>
2023-09-13 17:15:51 +0530

    first commit

---
+++ assets/scripts/global.js
@@ -0,0 +1,65 @@
+// SIMPLE CALCULATE FUNCTION
+const addBtn = document.getElementById("add-btn");
+const firstNumber = document.getElementById("first-number");
+const secondNumber = document.getElementById("second-number");
+const answer = document.getElementById("calc-answer");
+
+//ADD BUTTON USING PURE DOM
+addBtn.addEventListener("click", () => {
+  if (firstNumber.value !== "" && secondNumber.value !== "") {
+    answer.innerHTML =
+      parseInt(firstNumber.value) + parseInt(secondNumber.value);
+    firstNumber.value = "";
+    secondNumber.value = "";
+  } else {
+    alert("Enter the number plz");
+  }
+});
+
+function deleteBtn() {
+  const answer = document.getElementById("calc-answer");
+  if (firstNumber.value !== "" && secondNumber.value !== "") {
+    answer.innerHTML =
+      parseInt(firstNumber.value) - parseInt(secondNumber.value);
+    firstNumber.value = "";
+    secondNumber.value = "";
+  } else {
+    alert("Enter the number plz");
+  }
+}
+function multiplyBtn() {
+  const answer = document.getElementById("calc-answer");
+  if (firstNumber.value !== "" && secondNumber.value !== "") {
+    answer.innerHTML =
+      parseInt(firstNumber.value) * parseInt(secondNumber.value);
+    firstNumber.value = "";
+    secondNumber.value = "";
+  } else {
+    alert("Enter the number plz");
+  }
+}
+
+function divideBtn() {
+  const answer = document.getElementById("calc-answer");
+  if (firstNumber.value !== "" && secondNumber.value !== "") {
+    answer.innerHTML =
+      parseInt(firstNumber.value) / parseInt(secondNumber.value);
+    firstNumber.value = "";
+    secondNumber.value = "";
+  } else {
+    alert("Enter the number plz");
+  }
+}
+
+// CALCULATE THE SQUARE OF A NUMBER
+function calculateSquare() {
+  const inputElement = document.getElementById("numberInput");
+  const number = parseFloat(inputElement.value);
+  if (!isNaN(number)) {
+    const square = number * number;
+    const resultElement = document.getElementById("result");
+    resultElement.innerHTML = "The square of " + number + " is " + square;
+  } else {
+    alert("Please enter a valid number.");
+  }
+}
